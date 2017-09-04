4754b005a326e02eaa43054ace17423e2cdd8c67
Loke Carlsson <lc222ak@student.lnu.se>
2017-09-04 13:43:57 +0200

    Finished arrays

--- src/lc222ak_assign1/Arrays.java
+++ src/lc222ak_assign1/Arrays.java
@@ -2,6 +2,92 @@ package lc222ak_assign1;
 
 public class Arrays {
     public static void main(String [] args) throws Error {
-        
+    }
+
+    static int sum(int[] arr) {
+        int result = 0;
+        for (int anArr : arr) {
+            result += anArr;
+        }
+        return result;
+    }
+
+    static String toString(int[] arr) {
+        StringBuilder strBuilder = new StringBuilder();
+        for (int anArr : arr) {
+            strBuilder.append(anArr);
+        }
+
+        return strBuilder.toString();
+    }
+
+    static int[] addN(int[] arr, int n) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += n;
+        }
+        return arr;
+    }
+
+    static int[] reverse(int[] arr) {
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[arr.length - 1 - i];
+        }
+        return newArr;
+    }
+
+    static void replaceAll(int[] arr, int old, int nw) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == old) {
+                arr[i] = nw;
+            }
+        }
+    }
+
+    static int[] sort(int[] arr) {
+        int[] newArr = new int[arr.length];
+        int holder = 0;
+        System.arraycopy( arr, 0, newArr, 0, arr.length );
+
+        for (int i = 0; i < newArr.length; i++) {
+            for (int j = 1; j < newArr.length - i; j++) {
+                if (newArr[j - 1] > newArr[j]) {
+                    holder = newArr[j-1];
+                    newArr[j - 1] = newArr[j];
+                    newArr[j] = holder;
+                }
+            }
+        }
+        return newArr;
+    }
+
+    static boolean hasSubsequence(int[] arr, int[] sub) {
+        int counter = 0;
+
+        for (int el : arr) {
+            if (el == sub[counter]) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if (counter == sub.length) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int[] absDif(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            throw new Error("Both arrays must be same length");
+        }
+        int[] newArr = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            newArr[i] = arr1[i] - arr2[i];
+            if (newArr[i] < 0) {
+                newArr[i] = -newArr[i];
+            }
+        }
+        return newArr;
     }
 }
---
+++ src/lc222ak_assign1/ArraysMain.java
@@ -0,0 +1,65 @@
+package lc222ak_assign1;
+
+import lc222ak_assign1.*;
+
+public class ArraysMain {
+    public static void main(String [] args) throws Error {
+        sum();
+        string();
+        addN();
+        reverse();
+        replace();
+        sort();
+        subSequence();
+        absDif();
+    }
+
+    private static void sum() {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("Sum: " + Arrays.sum(arr));
+    }
+
+    private static void string() {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("toString: " + Arrays.toString(arr));
+    }
+
+    private static void addN() {
+        int[] arr = {1,2,3,4,5};
+        int[] res = Arrays.addN(arr, 5);
+        System.out.println("Add number: " + Arrays.toString(res));
+    }
+
+    private static void reverse() {
+        int[] arr = {1,2,3,4,5};
+        int[] res = Arrays.reverse(arr);
+        System.out.println("Reverse: " + Arrays.toString(res));
+    }
+
+    private static void replace() {
+        int[] arr = {1,2,3,4,5};
+        Arrays.replaceAll(arr, 5, 7);
+        System.out.println("Replace all: " + Arrays.toString(arr));
+    }
+
+    private static void sort() {
+        int[] arr = {1,5,3,4,2};
+        int[] res = Arrays.sort(arr);
+        System.out.println("Sort array: " + Arrays.toString(res));
+    }
+
+    private static void subSequence() {
+        int[] arr = {1,5,3,4,2};
+        int[] sub = {5,3,4};
+        boolean res = Arrays.hasSubsequence(arr, sub);
+        System.out.println("Has sub sequence?: " + res);
+    }
+
+    private static void absDif() {
+        int[] arr1 = {1,5,4,3,2,-5,10,-4};
+        int[] arr2 = {1,1,1,1,1,1,1,1};
+        int[] res = Arrays.absDif(arr1, arr2);
+        System.out.println("Absolute difference: " + Arrays.toString(res));
+    }
+
+}
