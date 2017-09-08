e2fb688a76a6d632b400f6b3e6f58504eefc34f0
Loke Carlsson <lc222ak@student.lnu.se>
2017-09-08 13:33:28 +0200

    Finished histogram

--- src/lc222ak_assign1/Histogram.java
+++ src/lc222ak_assign1/Histogram.java
@@ -3,7 +3,8 @@ package lc222ak_assign1;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Histogram {
     public static void main(String URL) throws Error {
@@ -11,25 +12,58 @@ public class Histogram {
             File file = new File(URL);
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String text;
-            int uppercase = 0;
+            int[] result = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            Pattern p = Pattern.compile("-?\\d+");
             while((text=reader.readLine()) != null){
-                for(int i = 0; i < text.length(); i++) {
-
+                Matcher m = p.matcher(text);
+                while(m.find()) {
+                    int textInt = Integer.parseInt(m.group());
+                    if (textInt >= 1 && textInt <= 10) {
+                        result[0]++;
+                    } else if (textInt < 20) {
+                        result[1]++;
+                    } else if (textInt < 30) {
+                        result[2]++;
+                    } else if (textInt < 40) {
+                        result[3]++;
+                    } else if (textInt < 50) {
+                        result[4]++;
+                    } else if (textInt < 60) {
+                        result[5]++;
+                    } else if (textInt < 70) {
+                        result[6]++;
+                    } else if (textInt < 80) {
+                        result[7]++;
+                    } else if (textInt < 90) {
+                        result[8]++;
+                    } else if (textInt < 100) {
+                        result[9]++;
+                    } else if (textInt < 200) {
+                        result[10]++;
+                    }
                 }
             }
-            System.out.println("1 - 10 | " + uppercase);
-            System.out.println("11 - 20 | " + uppercase);
-            System.out.println("21 - 30 | " + uppercase);
-            System.out.println("31 - 40 | " + uppercase);
-            System.out.println("41 - 50 | " + uppercase);
-            System.out.println("51 - 60 | " + uppercase);
-            System.out.println("61 - 70 | " + uppercase);
-            System.out.println("71 - 80 | " + uppercase);
-            System.out.println("81 - 90 | " + uppercase);
-            System.out.println("91 - 100 | " + uppercase);
-            System.out.println("101 - 200 | " + uppercase);
-        } catch (IOException e) {
+            print("1 - 10    | ", result[0]);
+            print("11 - 20   | ", result[1]);
+            print("21 - 30   | ", result[2]);
+            print("31 - 40   | ", result[3]);
+            print("41 - 50   | ", result[4]);
+            print("51 - 60   | ", result[5]);
+            print("61 - 70   | ", result[6]);
+            print("71 - 80   | ", result[7]);
+            print("81 - 90   | ", result[8]);
+            print("91 - 100  | ", result[9]);
+            print("101 - 200 | ", result[10]);
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void print(String range, int number) {
+        StringBuilder stars = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            stars.append("*");
+        }
+        System.out.println(range + stars);
     }
 }
