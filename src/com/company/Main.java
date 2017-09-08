27acfbcb1a685e630bf60db277e377fe521475be
Loke Carlsson <lc222ak@student.lnu.se>
2017-09-08 09:02:50 +0200

    Moving workspace

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ import lc222ak_assign1.*;
 public class Main {
 
     public static void main(String[] args) {
-        CountChars.main("/home/loke/Documents/school/2DV600/src/SergioGarciaBeatsTheAmericans.txt");
+        Histogram.main("/home/loke/Documents/school/2DV600/src/Integers.txt");
     }
 }
--- src/lc222ak_assign1/Histogram.java
+++ src/lc222ak_assign1/Histogram.java
@@ -1,7 +1,35 @@
 package lc222ak_assign1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Histogram {
-    public static void main(String [] args) throws Error {
-        
+    public static void main(String URL) throws Error {
+        try {
+            File file = new File(URL);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String text;
+            int uppercase = 0;
+            while((text=reader.readLine()) != null){
+                for(int i = 0; i < text.length(); i++) {
+
+                }
+            }
+            System.out.println("1 - 10 | " + uppercase);
+            System.out.println("11 - 20 | " + uppercase);
+            System.out.println("21 - 30 | " + uppercase);
+            System.out.println("31 - 40 | " + uppercase);
+            System.out.println("41 - 50 | " + uppercase);
+            System.out.println("51 - 60 | " + uppercase);
+            System.out.println("61 - 70 | " + uppercase);
+            System.out.println("71 - 80 | " + uppercase);
+            System.out.println("81 - 90 | " + uppercase);
+            System.out.println("91 - 100 | " + uppercase);
+            System.out.println("101 - 200 | " + uppercase);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
