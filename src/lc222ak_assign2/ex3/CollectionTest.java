f37529fdcc29b1dbe1c607c08ebb69088a1e33d1
Loke Carlsson <lc222ak@student.lnu.se>
2017-09-18 10:25:35 +0200

    Added more tests

--- src/lc222ak_assign2/ex3/CollectionTest.java
+++ src/lc222ak_assign2/ex3/CollectionTest.java
@@ -17,4 +17,38 @@ public class CollectionTest {
         myList.addAt(5, 0);
         assert(myList.size() == 2);
     }
+    @Test
+    public void shouldReplaceAtIndex() {
+        ArrayIntList myList = new ArrayIntList();
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+        }
+        myList.addAt(10, 2);
+        assert(myList.get(2) == 10);
+    }
+    @Test
+    public void shouldRemoveAtIndex() {
+        ArrayIntList myList = new ArrayIntList();
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+        }
+        myList.remove(2);
+        assert(myList.get(2) == 0);
+    }
+    @Test
+    public void shouldGetAtIndex() {
+        ArrayIntList myList = new ArrayIntList();
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+        }
+        assert(myList.get(2) == 2);
+    }
+    @Test
+    public void shouldFindIndex() {
+        ArrayIntList myList = new ArrayIntList();
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+        }
+        assert(myList.indexOf(2) == 2);
+    }
 }
