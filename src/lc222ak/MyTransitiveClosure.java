7eef6ff38bfe54ddbbca4cb4c6679f90d59396a3
Loke Carlsson <lc222ak@student.lnu.se>
2017-10-08 20:03:49 +0200

    Finished Transitive closure

--- src/lc222ak/MyTransitiveClosure.java
+++ src/lc222ak/MyTransitiveClosure.java
@@ -4,11 +4,18 @@ import graphs.DirectedGraph;
 import graphs.Node;
 import graphs.TransitiveClosure;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 public class MyTransitiveClosure<E> implements TransitiveClosure<E> {
     public Map<Node<E>, Collection<Node<E>>> computeClosure(DirectedGraph<E> dg) {
-        return null;
+        Map<Node<E>, Collection<Node<E>>> map = new HashMap<>();
+        MyDFS<E> dfs = new MyDFS<>();
+
+        for (Node<E> node : dg) {
+            List<Node<E>> nodes = dfs.dfs(dg, node);
+            map.put(node, nodes);
+        }
+
+        return map;
     }
 }
